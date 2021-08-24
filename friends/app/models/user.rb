8608f81e14976e5025ef86530cf295d8653bb5b9
Jacob Sexton <jshuman@protonmail.com>
2021-08-24 01:58:56 +0000

    Populated DB. Added script.

--- friends/app/models/user.rb
+++ friends/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-    has_many :friends, class_name: "User", foreign_key:"user_id"
+    has_many :friendships, :foreign_key => "user_id", :class_name => "Friendship"
+    has_many :friends, :through => :friendships
 end
